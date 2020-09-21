5ff09921975f00ed90f4dc87bba71656cb49b2f4
Sonali Najera <sonalitzul@gmail.com>
2020-09-21 14:36:34 -0700

    solved for binary and anagrams

--- Recursion.js
+++ Recursion.js
@@ -67,7 +67,7 @@ const nTriangularNum = (num) => {
     return num;
   }
   else {
-   return num + nTriangularNum(num - 1)
+    return num + nTriangularNum(num - 1)
   }
 }
 
@@ -87,16 +87,16 @@ const strSplitter = (str, splitter) => {
   //recursive case
   let splitterIndex = str.indexOf(splitter)
 
-  let newStr  = str.substr(0, splitterIndex);
- 
+  let newStr = str.substr(0, splitterIndex);
+
   let endOfStr = str.substr(splitterIndex + 1, str.length);
 
   newArr.push(newStr)
 
- return newArr.concat(strSplitter(endOfStr, splitter)); 
+  return newArr.concat(strSplitter(endOfStr, splitter));
 }
 
-console.log(strSplitter('a quick brown fox', ' '));
+// console.log(strSplitter('a quick brown fox', ' '));
 
 //6. Fibonacci
 const fibonnaci = (num) => {
@@ -107,7 +107,7 @@ const fibonnaci = (num) => {
     return 1;
   }
 
-  return fibonnaci(num - 1) + fibonnaci(num - 2);  
+  return fibonnaci(num - 1) + fibonnaci(num - 2);
 }
 
 // console.log(fibonnaci(7));
@@ -135,7 +135,7 @@ let mySmallMaze = [
 function pathFinder(maze) {
   this.maze = maze
 
-  this.traverse = function(column , row) {
+  this.traverse = function (column, row) {
     if (this.maze[column][row] === 'e') {
       console.log('Solution at ' + column + row);
       return;
@@ -144,19 +144,28 @@ function pathFinder(maze) {
       if (column < this.maze.length - 1) {
         this.traverse(column + 1, row);
       }
-      if(row < this.maze[column].length - 1) {
+      if (row < this.maze[column].length - 1) {
         this.traverse(column, row + 1);
       }
-      if(column > 0) {
+      if (column > 0) {
         this.traverse(column - 1, row);
       }
-      if(row > 0) {
+      if (row > 0) {
         this.traverse(column, row - 1);
       }
     }
 
   }
 }
+
+// traverse = (maze, pos, steps ='') =>{
+//   if (pos is exist) {return steps}
+//     return (canGoUp && traverse(maze, posR, steps + 'U')) ||
+//     (canGoRight && traverse(maze, posR, steps + 'R')) ||
+//     (canGoDown && traverse(maze, posR, steps + 'D')) ||
+//     (canGoLeft && traverse(maze, posR, steps + 'L'))
+  
+// }
 
 // var ms = new pathFinder(mySmallMaze)
 // console.log(ms.traverse(0,0))
@@ -185,15 +194,165 @@ function pathFinder(maze) {
 
 // findAllAnagrams('east');
 
+const findAllAnagrams = (word) => {
+// base case
+if (word.length < 2) {
+  return [word];
+}
 
-function anagramGrouper(words){
+if (word.length === 2) {
+  return [word, word[1] + word[0]]
+}
+let anagramArray = []
+
+for (let i = 0; i < word.length; i++) {
+  let firstChar = word[i]; // 'a'
+  let remainingChar = word.slice(0, i) + word.slice(i + 1)//'bc' 
+  anagramArray = anagramArray.concat(findAllAnagrams(remainingChar).map(charSet => firstChar + charSet))// [bc, cb]
+}
+
+return anagramArray;
+
+}
+
+// console.log(findAllAnagrams('east'), findAllAnagrams('east').length)
+
+// [abc, acb, bac, bca, cab, cba]
+function anagramGrouper(words) {
   const anagrams = {};
-  words.forEach((word)=>{
-      const sortedWord = alphabetize(word);
-      if (anagrams[sortedWord]) {
-          return anagrams[sortedWord].push(word);
-      }
-      anagrams[sortedWord] = [word];
+  words.forEach((word) => {
+    const sortedWord = alphabetize(word);
+    if (anagrams[sortedWord]) {
+      return anagrams[sortedWord].push(word);
+    }
+    anagrams[sortedWord] = [word];
   });
   return anagrams;
 }
+
+
+
+// // 11. Org chart 
+// let orgChart = {
+//   Zuckerberg: [
+//     {
+//       Schroepfer: [
+//         {
+//           Bosworth:
+//             [
+//               Steve,
+//               Kyle,
+//               Andra,
+//             ]
+//         },
+//         {
+//           Zhao:
+//             [
+//               Richie,
+//               Sofia,
+//               Jen,
+//             ]
+//         }
+//       ]
+//     },
+//     {
+//       Schrage: [
+//         {
+//           VanDyck:
+//             [
+//               Sabrina,
+//               Michelle,
+//               Josh
+//             ]
+//         },
+//         {
+//           Swain:
+//             [
+//               Blanch,
+//               Tom,
+//               Joe,
+//             ]
+//         }
+//       ]
+//     },
+//     {
+//       Sandberg:
+//         [
+//           {
+//             Goler:
+//               [
+//                 Eddie,
+//                 Julie,
+//                 Annie,
+//               ]
+//           },
+//           {
+//             Hernandez:
+//               [
+//                 Rowi,
+//                 Inga,
+//                 Morgan,
+//               ]
+//           },
+//           {
+//             Moissinac:
+//               [
+//                 Amy,
+//                 Chuck,
+//                 Vinni,
+//               ]
+//           },
+//           {
+//             Kelley:
+//               [
+//                 Eric,
+//                 Ana,
+//                 Wes,
+//               ]
+//           }
+//         ]
+//     }
+//   ]
+// }
+
+// let babyOrg =  {
+//   Kelley:
+//     [
+//       Eric,
+//       Ana,
+//       Wes,
+//     ]
+// }
+
+// const printOrgChart = (chart) => {
+//   // base case 
+  
+//   // recursion 
+// }
+
+//12. Binary Representation 
+
+// 8 4 2 1
+// 0 0 0 0  
+// 0 0 0 1
+// 0 0 1 0 
+// 0 0 1 1 
+
+
+
+const getBinaryValue = (num) => {
+ let binary = ''
+  if (num === 0 ) {
+    return binary + '0';
+  }
+
+  if (num === 1) {
+    return binary + '1';
+  }
+ 
+  return binary + getBinaryValue(Math.floor(num / 2)) + num % 2;
+  // '' + 1 + 1  = 1 1 
+
+}
+
+// console.log(getBinaryValue(25));
